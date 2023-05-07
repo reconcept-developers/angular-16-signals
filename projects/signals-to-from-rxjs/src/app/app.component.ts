c65d83b0c8f7c1e02aab813ca3947332ec9df68f
Gerhard <gerhard@reconcept.nl>
2023-05-07 20:03:33 +0200

    add http call to method

--- projects/signals-to-from-rxjs/src/app/app.component.ts
+++ projects/signals-to-from-rxjs/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { map } from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -26,5 +27,12 @@ export class AppComponent {
 
 
   find() {
+  }
+
+  getShips(ship: string) {
+    const query = `?search=${ship}`
+    return this.http.get(`https://swapi.dev/api/starships/${query}`).pipe(
+      map((response: any) => response.results as any[])
+    )
   }
 }
